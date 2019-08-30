814e95d163c8b396019448ffb354285c7ce4ddec
aryaggg <30399811+aryaggg@users.noreply.github.com>
2019-08-30 17:34:04 +0700

    tes3

--- application.py
+++ application.py
@@ -11,6 +11,7 @@ def show():
                 <head>
                     <title>Analyze Sample</title>
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+                    <script src="azure-storage.blob.min.js" charset="utf-8"></script>
                 </head>
                 <body>
                 
