96875a3692efba04ef3d6ec1b823c1f0cfc60216
aryaggg <30399811+aryaggg@users.noreply.github.com>
2019-09-01 17:54:20 +0700

    ts7

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask,Response,request,render_template,redirect,jsonify  
 from flask_cors import CORS
 import pyodbc
-import azure.common
-from azure.storage import CloudStorageAccount
+#import azure.common
+#from azure.storage import CloudStorageAccount
 
 app = Flask(__name__)
 
